2802bbccc8098963ca646ebe162070aac7b4ecca
EZTest AI <eztest.ai@commit.com>
2025-04-05 17:30:56 +0530

    Added unit test file test/db.test.js

---
+++ test/db.test.js
@@ -0,0 +1,13 @@
+const db = require('../db');
+
+describe('Database', () => {
+  it('should connect to the database', async () => {
+    const connection = await db.connect();
+    expect(connection).toBeTruthy();
+  });
+
+  it('should fetch users from the database', async () => {
+    const users = await db.getUsers();
+    expect(Array.isArray(users)).toBe(true);
+  });
+});
