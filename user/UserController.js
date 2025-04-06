cf97ae8f465b1dd9b2c42488567db15b023a68bc
anand-belsterns <anand.palanisamy@belsterns.com>
2025-04-06 20:06:43 +0530

    Update UserController.js

--- user/UserController.js
+++ user/UserController.js
@@ -14,7 +14,7 @@ router.post('/', function (req, res) {
             password : req.body.password
         }, 
         function (err, user) {
-            if (err) return res.status(500).send("There was a problem  adding the information to the database.");
+            if (err) return res.status(500).send("There was a problem  adding the  information to the database.");
             res.status(200).send(user);
         });
 });
