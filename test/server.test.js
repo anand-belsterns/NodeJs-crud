9c4846996c063c3a46c73a0232c2a4ebcd66a077
EZTest AI <eztest.ai@commit.com>
2025-04-07 23:20:54 +0530

    Added test/server.test.js file

---
+++ test/server.test.js
@@ -0,0 +1,14 @@
+const request = require('supertest');
+const app = require('../server');
+
+describe('Server', () => {
+    it('should respond with a 200 status code', async () => {
+        const response = await request(app).get('/');
+        expect(response.statusCode).toBe(200);
+    });
+
+    it('should return JSON', async () => {
+        const response = await request(app).get('/api/data');
+        expect(response.headers['content-type']).toEqual(expect.stringContaining('json'));
+    });
+});
